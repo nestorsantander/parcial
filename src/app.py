8f51764852159b905aadbb06555902fc0591cc88
Nestor Sosa Santander <nestorsantander@hotmail.com>
2023-06-15 09:42:21 -0300

    corregimos redundancia y cambiamos texto

--- src/app.py
+++ src/app.py
@@ -1,17 +1,17 @@
 from flask import Flask
 
-var = "Hola mundo"
-var = "Hola mundo"
-var = "Hola mundo"
-var2 = var
-var3 = var2
-var4 = var3
+#var = "Sandwich de entraña en el falso asado."
+#var = "Sandwich de entraña en el falso asado."
+#var = "Sandwich de entraña en el falso asado."
+#var2 = var
+#var3 = var2
+#var4 = var3
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return var4
+    return "Sandwich de entraña en el falso asado."
 
 if __name__ == "__main__":
      app.run()
