76e2149678de75f9412392ec57b4151b0a6ff374
Nestor Sosa Santander <nestorsantander@hotmail.com>
2023-06-15 09:35:42 -0300

    corregimos pero con redundancia

--- src/app.py
+++ src/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
+var = "Hola mundo"
+var = "Hola mundo"
+var = "Hola mundo"
+var2 = var
+var3 = var2
+var4 = var3
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hola mundo8"
+    return var4
 
 if __name__ == "__main__":
      app.run()
